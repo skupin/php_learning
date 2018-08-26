618c0677ba9b40a9cc7d58f6d9d44f75e663b2c6
Max Skupin <maxskupin@gmail.com>
2018-08-26 18:07:03 -0400

    done module 6

--- _practical-app/7.php
+++ _practical-app/7.php
@@ -16,24 +16,59 @@
 	
 	
 	
-	<?php
+        <?php
 
-    // this is a change
+        // this is a change
 
-	/*  Step 1 - Create a database in PHPmyadmin
+        /*  Step 1 - Create a database in PHPmyadmin
 
-		Step 2 - Create a table like the one from the lecture
+            Step 2 - Create a table like the one from the lecture
 
-		Step 3 - Insert some Data
+            Step 3 - Insert some Data
 
-		Step 4 - Connect to Database and read data
+            Step 4 - Connect to Database and read data
 
-*/
-	
-	?>
+    */
+
+        ?>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Username</th>
+                    <th scope="col">Password</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    $connect = mysqli_connect('localhost','root','447713001','php_learning');
+                    if (!$connect){
+                        die("Database connection failed. " . mysqli_connect_error());
+                    } else {
+                        echo "<h4>We are connected</h4>";
+                    }
 
 
+                    $query = "SELECT * FROM users";
+                    $result = mysqli_query($connect,$query);
+                    if (!$result) {
+                        die('Query failed! ..' . mysqli_error());
+                    }
+                    while($row = mysqli_fetch_assoc($result)){
+                        $id = $row['id'];
+                        $user = $row['username'];
+                        $pass = $row['password'];
+                        echo "<tr>";
+                        echo "<th scope='row'>$id</th>";
+                        echo "<td>$user</td>";
+                        echo "<td>$pass</td>";
+                        echo "</tr>";
 
+                    }
+                ?>
+            </tbody>
+        </table>
 
 
 </article><!--MAIN CONTENT-->
