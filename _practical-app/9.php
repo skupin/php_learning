481f85cdc44c4a6fb7c5e0e271f1ec6b2198696a
maxskupin@gmail.com <Kadmos.447713001>
2018-09-02 22:11:42 -0400

    lession 9 done

--- _practical-app/9.php
+++ _practical-app/9.php
@@ -1,37 +1,59 @@
+<?php session_start();
+$_SESSION['givBacon'] = "Bacon ipsum dolor amet tri-tip salami ground round hamburger. Salami shank meatball, ground round spare ribs chuck pig short loin kevin buffalo t-bone. Ball tip meatloaf landjaeger leberkas. Filet mignon shank brisket frankfurter kielbasa picanha. Leberkas alcatra andouille, swine drumstick pork shoulder.";
+?>
 <?php include "functions.php"; ?>
-<?php include "includes/header.php";?>
+<?php include "includes/header.php"; ?>
 
 
+    <section class="content">
 
-	<section class="content">
+    <aside class="col-xs-4">
 
-		<aside class="col-xs-4">
-
-		<?php Navigation();?>
-			
-			
-		</aside><!--SIDEBAR-->
+        <?php Navigation(); ?>
 
 
-			<article class="main-content col-xs-8">
-			
-		
-	
-	<?php 
-
-	/*  Create a link saying Click Here, and set 
-	the link href to pass some parameters and use the GET super global to see it
-
-		Step 2 - Set a cookie that expires in one week
-
-		Step 3 - Start a session and set it to value, any value you want.
-	*/
-	
-	?>
+    </aside><!--SIDEBAR-->
 
 
+    <article class="main-content col-xs-8">
 
 
+        <?php
 
-</article><!--MAIN CONTENT-->
+        /*  Create a link saying Click Here, and set
+        the link href to pass some parameters and use the GET super global to see it*/
+
+        $name = "this is a string with spaces";
+        $buttonText = "CLICK HERE";
+
+        ?>
+
+        <a href="9.php?name=<?php echo $name; ?>"><?php echo $buttonText; ?></a>
+
+        <?php print_r($_GET); ?>
+        <?php /* Step 2 - Set a cookie that expires in one week*/ ?>
+
+        <?php
+
+        if (!isset($_COOKIE['myFirstCookie'])) {
+            $cookieName = "myFirstCookie";
+            $cookieValue = "kJSDNFBkjSNDFkj;SNDFkjs";
+
+            $cookieExpiration = time() + (60 * 60 * 24 * 7);
+
+            setcookie($cookieName, $cookieValue, $cookieExpiration);
+
+        } else {
+            echo $_COOKIE['myFirstCookie'];
+        }
+
+        ?>
+
+        <?php /* Step 3 - Start a session and set it to value, any value you want.*/
+        echo $_SESSION['givBacon'];
+
+        ?>
+
+
+    </article><!--MAIN CONTENT-->
 <?php include "includes/footer.php"; ?>
